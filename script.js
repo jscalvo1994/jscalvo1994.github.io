bfaefe9a1f4150463035768345750b8c030d65c2
jscalvo1994 <juansebastian.calvo408@comunidadunir.net>
2024-04-01 19:41:50 -0500

    Nuevas imagenes, personalizando portafolio

--- script.js
+++ script.js
@@ -21,16 +21,16 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
+        habilidades[0].classList.add("js-htmlcss");
+        habilidades[1].classList.add("frameworksjs");
+        habilidades[2].classList.add("Python");
+        habilidades[3].classList.add("QA");
+        habilidades[4].classList.add("SQL");
         habilidades[5].classList.add("comunicacion");
         habilidades[6].classList.add("trabajo");
         habilidades[7].classList.add("creatividad");
         habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        habilidades[9].classList.add("Leadership");
     }
 }
 
